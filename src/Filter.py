68effafea25749bebc8422c336cd1961b23ffd67
Roboti-dlia-zvezd <alexander.demishkevich@gmail.com>
2024-10-05 16:02:46 -0400

    created template files for STA & LTA algorithm & filter

---
+++ src/Filter.py
@@ -0,0 +1,16 @@
+import os
+from string import printable
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as scipint
+
+
+
+##Writte a couple filter functions that will take raw data and filter out noise. 
+##The output of those function will be returned as an ouput
+##The output from multiple filter functions will be compared based on an evaluation
+##criteria and the best output data will be chosen to be used further in the code
+
---
+++ src/STA_LTA_algo.py
@@ -0,0 +1,8 @@
+##Input to STA algo is data filtered. 
+##The algorithm should create the characteristic function so you can pick a threshold
+##that allows you to get the arrival and end times of the quake
+##Need to find a threshold that will work for every signal
+##Make the threshold adaptable to different types of signals 
+##Try out with different types of data
+##Try different algorithms to see what gives best results based on criteria(tbd)
+##to get the best and most accurate start and end times. 
